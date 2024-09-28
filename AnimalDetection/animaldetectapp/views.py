b7873607f75b0140320d3f78dae8520330b4182e
Abdulla Abdul Assis <132735918+ArtIntWorld@users.noreply.github.com>
2024-09-28 10:52:22 +0530

    Initial commit msg

---
+++ AnimalDetection/animaldetectapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import*
+
+# Create your views here.
+def login(request):
+    return render(request,"public/login.html")
+
+def signup(request):
+    return render(request,'public/signup.html')
+
+def forestdivision(request):
+    return render(request,'admin/forestdivision.html')
+
+def foreststation(request):
+    return render(request,'admin/foreststation.html')
+
+def animal(request):
+    return render(request,'admin/animal.html')
+
+def reply(request):
+    return render(request,'admin/reply.html')
+
+def complaintcheck(request):
+    return render(request,'forest_station/complaintcheck.html')
+
+def alertnotify(request):
+    return render(request,'forest_station/alertnotify')
